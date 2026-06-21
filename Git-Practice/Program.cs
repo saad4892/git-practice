c1bbd4862bb93f8b26702720a7b484bb0cd9222d
AdeelHassan110 <adeellagharibaloch@gmail.com>
2026-06-21 15:07:21 +0500

    Added new comment in Program.cs

--- Git-Practice/Program.cs
+++ Git-Practice/Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 //changes for push..
 // Add services to the container.
 
+// Added new feature for fetching data
+Console.WriteLine("Hello from master branch!");
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
