bbcacdd6ab1f261d472943360bc6cdab671d7b4d
4892@Leghari <m.saadbaloch4892@gmail.com>
2026-06-21 11:51:04 +0500

    New Controller added to master

---
+++ Git-Practice/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Git_Practice.Controllers
+{
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
